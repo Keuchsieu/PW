a59b4250cca7f7cb51b79d9feab584687302be59
yuqingqiu <yuqingqiu93@gmail.com>
2019-05-25 19:37:50 -0700

    Get things connected

---
+++ src/app/components/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './Footer.js';
+
+
+function MainPage(props){
+  return(
+      <div className="MainPage">
+
+        <p>MainPage content here</p>
+        <Footer />
+      </div>
+  );
+}
+
+export default MainPage;
