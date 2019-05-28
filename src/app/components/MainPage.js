bfefb68c38bbb237dbc852b1c8188b2528bb8d52
yuqingqiu <yuqingqiu93@gmail.com>
2019-05-28 11:06:34 -0700

    Added Header

--- src/app/components/MainPage.js
+++ src/app/components/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer.js';
+import Header from './Header.js';
 
 
 function MainPage(props){
   return(
       <div className="MainPage">
+        <Header />
         <p>MainPage content here</p>
         <Footer />
       </div>
