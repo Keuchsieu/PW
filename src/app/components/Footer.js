ca3c7a9c6af674454413868ed7b11aa1c2655336
yuqingqiu <yuqingqiu93@gmail.com>
2019-05-30 19:02:28 -0700

    Added a bit style

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,8 +5,8 @@ function Footer(props){
   return(
     <div className="Footer">
       <div className="contact">
-        <p className="contact_title">CONTACT</p>
-        <p className="contact_email">email: yuqingqiu93@gmail.com</p>
+        <p className="contact_title titles">CONTACT</p>
+        <p className="contact_email">link to linkedIn and github</p>
       </div>
       <p className="copy-right">Copyright @ {now.getFullYear()} by Yuqing Qiu. All rights reserved.</p>
     </div>
--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,10 +2,17 @@ import React from 'react';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown';
 
+var yqStyle = {
+  display: 'inline-block',
+  textAlign: 'center',
+  margin: 'auto'
+}
+
 function Header(props){
   return(
     <div className="Header">
-      <DropdownButton title="testing dropdown">
+      <h3 className="titles" style={yqStyle}>Yuqing Qiu</h3>
+      <DropdownButton className="menu" title="testing dropdown">
         <Dropdown.Item>content 1</Dropdown.Item>
         <Dropdown.Item>2</Dropdown.Item>
       </DropdownButton>
